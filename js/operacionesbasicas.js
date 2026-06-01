f59c2169e4e51d6fffb5d5f734eca41cc5c4e181
dilanmoreno0706-hub <dilan.moreno0706@gmail.com>
2026-06-01 09:27:43 -0500

    operaciones basicas

---
+++ js/operacionesbasicas.js
@@ -0,0 +1,19 @@
+// calcular el area y perimetro de un triangulo
+const base = 8;
+const altura = 5;
+const area = base * altura;
+const perimetro = 2 * (base + altura);
+console.log (`Área: ${area} cm2`);
+console.log (`Perímetro: ${perimetro} cm`);
+
+// saber si un numero es par o impar usando el operador %
+const numero = 7;
+const esPar = numero % 2 === 0;
+console.log(`¿${numero} es par?: ${esPar}`);
+
+//calcular el promedio de tres notas
+const nota1 = 80;
+const nota2 = 90;
+const nota3 = 70;
+const promedio = (nota1 + nota2 + nota3) / 3;
+console.log(`Promedio: ${promedio}`);
