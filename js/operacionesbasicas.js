bf39d04bdd93f024779ee56799136892b3e48cfa
dilanmoreno0706-hub <dilan.moreno0706@gmail.com>
2026-06-03 06:23:10 -0500

    verificador de accesos

--- js/operacionesbasicas.js
+++ js/operacionesbasicas.js
@@ -17,3 +17,45 @@ const nota2 = 90;
 const nota3 = 70;
 const promedio = (nota1 + nota2 + nota3) / 3;
 console.log(`Promedio: ${promedio}`);
+
+// area de un triangulo
+const base2 = 5;
+const altura2 = 7;
+const area2 = ( base2 + altura2) / 2;
+console.log( `Area2 de triangulo: ${area2} cm2`);
+
+//area de un circulo
+const radio2 = 6
+const area3 = Math.PI* (radio2 **2);
+console.log(`Area de corculo: ${area3} cm2`);
+
+// volumen de un cubo
+const lado = 3
+const volumen = lado ** 3
+console.log(`Volumen de cubo: ${volumen} cm3`);
+
+
+// verificador de acceso
+const edadInvitado = 18;
+const tieneEntrada = true;
+const esVip = true;
+
+
+const puedeEntrar = edadInvitado >= 18 && tieneEntrada;
+console.log(` puede entrar: ${puedeEntrar}`); 
+
+const accesoVip = puedeEntrar && esVip;
+console.log(`¿tiene acceso VIP?: ${accesoVip} usuario VIP`);
+
+
+const esMenor = !(puedeEntrar);
+if (puedeEntrar) {
+
+    console.log("Bienvendio");
+    console.log(`¿es menor de edad? ${esMenor}`);
+
+}else{
+
+    console.log("No puede entrar");
+    console.log(`¿es mayor de edad?: ${esMenor}`);
+}
