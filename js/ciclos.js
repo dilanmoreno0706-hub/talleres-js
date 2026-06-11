014404370523e7cb7db082e8c186bd21ccd26e62
dilanmoreno0706-hub <dilan.moreno0706@gmail.com>
2026-06-11 09:25:43 -0500

    ejercicios nuevos

--- js/ciclos.js
+++ js/ciclos.js
@@ -2,4 +2,16 @@
 for(let i = 0; i < 5; i++){
     console.log(`iteración : ${i}`);
 }
+//tabla de multiplicar
 
+const tabla = 4;
+for (let p = 1; p <= 10 ; p++){
+    console.log(`${tabla} * ${p} = ${tabla * p}`);
+}
+
+// contar de 10 a 1 (cuenta regresiva)
+for( let a =10 ; a >= 1; a--){
+    console.log(a);
+
+}
+console.log("¡Despegue!");
